7efe96a95681f25722a002eedce8f456fd84a1ee
Alexandre de Souza Eustaquio <alexeus.souza@gmail.com>
2022-07-15 10:20:14 -0300

    Update Curso balta.io

--- M01-A21/Program.cs
+++ M01-A21/Program.cs
@@ -3,18 +3,28 @@
 class Programa
 {
 
-    static void RealizarPagamento(double valor)
+    static void PagamentoBoleto(double valor)
     {
-        Console.WriteLine("Valor pago {0}", valor);
+        Console.WriteLine("\nPagamento Boleto: => {0}", valor);
+
+    }
+
+    static void PagamentoApplePay(double valor)
+    {
+        Console.WriteLine("\nPagamento ApplePay: => {0}", valor);
 
     }
 
     static void Main(string[] args)
     {
 
-        var pagar = new Pagamento.Pagar(RealizarPagamento); // passo para funcao delegate a classe que vai executar o metodo Pagar()
+        var pagar = new Pagamento.Pagar(PagamentoBoleto); // passo para funcao delegate a classe que vai executar o metodo Pagar()
         // classe deve ter o mesmo tipo de retorno e o mesmo tipo de passagem de parametro 
         pagar(125);
+
+        pagar = new Pagamento.Pagar(PagamentoApplePay); // passo para funcao delegate a classe que vai executar o metodo Pagar()
+        // classe deve ter o mesmo tipo de retorno e o mesmo tipo de passagem de parametro 
+        pagar(250);
     }
 
 }
