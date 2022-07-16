4b29f4d75dd19ce1569d0afb5af262064a9edfe7
Alexandre de Souza Eustaquio <alexeus.souza@gmail.com>
2022-07-16 13:40:35 -0300

    update balta.io

--- M01-A22/Program.cs
+++ M01-A22/Program.cs
@@ -1,54 +1,48 @@
-﻿class Program
+﻿using System;
+class Program
 {
-
     static void Main(string[] args)
     {
-        var room = new Room(3);
+        var room = new Room(3); // Room recebe a quantidade de assentos disponiveis na sala
 
-        room.RoomSoldOutEvent += OnRoomSoldOut; // Delegando OnRoomSoldOut para o metodo OnRoomSoldOut, sempre que o evento for disparado este metodo será chamado
+        room.RoomSoldOutEvent += OnRoomSoldOut; // Delegando RoomSoldOutEvent para o metodo OnRoomSoldOut, sempre que o evento for disparado este metodo será chamado
         room.ReserveSeat();
         room.ReserveSeat();
         room.ReserveSeat(); // reserva chamada 3 vezes para testar o metodo!!!
-
     }
 
-    static void OnRoomSoldOut(object sender, EventArgs e) // Evento delegado para este metodo
+    static void OnRoomSoldOut(object sender, EventArgs e) // Metodo delegado para manipular o evento
     {
-
         System.Console.WriteLine("\nSala cheia!!!!\n");
     }
 }
-
 public class Room
 {
-
-    public Room(int seats)
-    {
-        Seats = seats;
-        seatsInUse = 0;
-    }
-
     private int seatsInUse = 0;
     public int Seats { get; set; }
-
+    public Room(int seats) // Construtor da classe, recebe o paramentro na chamada  de Room() na classe principal
+    {
+        Seats = seats; // Seats recebe a quantidade de assentos informado na chamada do metodo na Main
+        seatsInUse = 0;
+    }
     public void ReserveSeat()
     {
-        seatsInUse++;
-        if (seatsInUse >= Seats) // quando a quantidade de assentos em uso for igual a quantidade de assentos disponiveis, evento será chamado
+        seatsInUse++; // Quantidade de assentos incrementada
+        if (seatsInUse >= Seats) // quando a quantidade de assentos em uso for igual a quantidade de assentos disponiveis, evento OnRoomSoldOut() será chamado
         {
             OnRoomSoldOut(EventArgs.Empty); // EventArgs vazio, pois neste exemplo nenhuma informação extra será necessaria.
+            // Iniciar o nome de eventos com AoAconteceralgo, exemplo AoClicar(), AoFicarSalaCheia(), AoChegarFInalArquivo()
         }
         else
         {
             System.Console.WriteLine("\nAssento reservado");
         }
     }
-    public event EventHandler RoomSoldOutEvent; // event é do tipo evento e EventHandler é o manipulador de eventos
-                                                //RoomSoldOut ocorre este evento sempre que a sala estiver cheia.
-
-    protected virtual void OnRoomSoldOut(EventArgs e) // Manipulador do evento RoomSoldOut
+    public event EventHandler RoomSoldOutEvent; // RoomSoldOutEvent é do tipo evento e EventHandler é o manipulador deste evento
+    //RoomSoldOut ocorre este evento sempre que a sala estiver cheia.
+    protected virtual void OnRoomSoldOut(EventArgs e) // Metodo manipulador do evento RoomSoldOutEvent
     {
-        EventHandler handler = RoomSoldOutEvent; // handler é do tipo manipulador de eventos e recebe RoomSoldOut, será delegado para outra função
-        handler?.Invoke(this, e); // Invoke invoca o evento RoomSoldOut. Invoke precisa de (Object sender, Eventargs e)
+        EventHandler handler = RoomSoldOutEvent; // handler é do tipo manipulador de eventos e recebe RoomSoldOutEvent, será delegado para outra função
+        handler.Invoke(this, e); // Invoke invoca o evento RoomSoldOut. Invoke precisa de (Object sender, Eventargs e)
     }
 }
---
+++ M01-A23/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace M01A23
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pessoa = new Pessoa();
+            var pessoaContexto = new ContextoDados<Pessoa>(); // passando para ContextoDados<Pessoa> uma entidade do tipo Pessoa
+            pessoaContexto.Salvar(pessoa); // Passando para o metodo salvar um objeto do tipo pessoa
+
+            var aluno = new Aluno();
+            var alunoContexto = new ContextoDados<Aluno>(); // passando para ContextoDados<Aluno> uma entidade do tipo Aluno
+            alunoContexto.Salvar(aluno); // Passando para o metodo salvar um objeto do tipo aluno
+
+            var professor = new Professor();
+            var professorContexto = new ContextoDados<Professor>(); // passando para ContextoDados<Aluno> uma entidade do tipo Professor
+            professorContexto.Salvar(professor); // Passando para o metodo salvar um objeto do tipo professor
+
+            var EntidadeContexto = new ContextoDados<Pessoa, Aluno, Professor>(); // passando para ContextoDados<> 3 tipos de entidade, 
+                                                                                  //metodo vai salvar na ordem descrita no contexto, neste caso Pessoa, Aluno e Professor
+
+            EntidadeContexto.Salvar(pessoa); // Passando para o metodo salvar um objeto do tipo pessoa
+            EntidadeContexto.Salvar(aluno); // Passando para o metodo salvar um objeto do tipo aluno
+            EntidadeContexto.Salvar(professor); // Passando para o metodo salvar um objeto do tipo professor
+        }
+    }
+
+    public class ContextoDados<T> // classe generica do tipo T de type, para qualquer entidade
+    {
+        public void Salvar(T modelo)
+        {
+            System.Console.WriteLine("\nSalvando!!!!");
+        }
+    }
+
+    public class ContextoDados<PE, AL, PR> // classe generica do tipo PE, AL, PR, para 3 tipos de entidade
+    {
+        public void Salvar(PE modelo)
+        {
+            System.Console.WriteLine("\nSalvando!!!!");
+        }
+        public void Salvar(AL modelo)
+        {
+            System.Console.WriteLine("\nSalvando!!!!");
+        }
+
+        public void Salvar(PR modelo)
+        {
+            System.Console.WriteLine("\nSalvando!!!!");
+        }
+    }
+
+    public class Pessoa { }
+    public class Aluno { }
+    public class Professor { }
+}
