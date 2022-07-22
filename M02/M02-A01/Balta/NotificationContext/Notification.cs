0266ced2ca847efca0d79fb815e078a5b9a035e2
Alexandre de Souza Eustaquio <alexeus.souza@gmail.com>
2022-07-22 10:02:02 -0300

    Update Notification.cs

--- M02/M02-A01/Balta/NotificationContext/Notification.cs
+++ M02/M02-A01/Balta/NotificationContext/Notification.cs
@@ -3,8 +3,8 @@ namespace Balta.NotificationContext
 
     public sealed class Notification // classe sealed para evitar que seja extendida por outras classes, as notificações devem ser feitas por esta classe e suas propriedades
     {
-        public string Property { get; set; }
-        public string Message { get; set; }
+        public string Property { get; set; } // propriedade que deu erro
+        public string Message { get; set; } // mensagem de erro
 
         public Notification()
         {
